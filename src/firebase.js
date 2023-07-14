aa7e64e8e70dc40b04e054d77bf27e75a551144c
haoyio <hong@lyft.com>
2023-07-13 23:46:25 -0700

    convert App.js to use function component

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,8 +11,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Get a reference to the database service and export the reference for other modules
+// get a reference to the database service and export the reference for other modules
 export const database = getDatabase(firebaseApp);
